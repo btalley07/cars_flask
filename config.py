55a5179057189295570e494f3716da41d062ca96
Blair Talley <btalley07@gmail.com>
2021-06-22 14:53:44 -0700

    day 2 commit

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 #give access to the project in any os, allow outside files/folders
 # to be added to the project from the base directory 
@@ -11,7 +12,9 @@ class Config():
         using environment variables where availables
         otherwise create the config variable if not done already
     """
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLACLCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'solite:///' + os.path.join(basedir, 'app.ob')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'solite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #turn off update messages from sqlalchemy
     
